41dc13105662de3cdbf54cf57365bf08f6e12819
FeilyZhang <fei472625@foxmail.com>
2019-03-10 21:20:42 +0800

    space indent

--- src/tech/feily/dataanalysis/demo/Main.java
+++ src/tech/feily/dataanalysis/demo/Main.java
@@ -6,20 +6,20 @@ import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Random random = new Random();
-		double[] datas = new double[10];
-		for (int i = 0; i < datas.length; i++) {
-			datas[i] = random.nextInt(10);
-		}
-		DescriptiveStatistics stats = new DescriptiveStatistics();
-		for( int i = 0; i < datas.length; i++) {
-	        stats.addValue(datas[i]);
-		}
-		double mean = stats.getMean();
-		double std = stats.getStandardDeviation();
-		double median = stats.getPercentile(50);
-		System.out.append(mean + "    " + std + "    " + median);
-	}
+    public static void main(String[] args) {
+        Random random = new Random();
+        double[] datas = new double[10];
+        for (int i = 0; i < datas.length; i++) {
+            datas[i] = random.nextInt(10);
+        }
+        DescriptiveStatistics stats = new DescriptiveStatistics();
+        for( int i = 0; i < datas.length; i++) {
+            stats.addValue(datas[i]);
+        }
+        double mean = stats.getMean();
+        double std = stats.getStandardDeviation();
+        double median = stats.getPercentile(50);
+        System.out.append(mean + "    " + std + "    " + median);
+    }
 
 }
